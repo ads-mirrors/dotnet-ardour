4e83c1a7db3331ac5f04eed99b1d682ea4be5de4
Ben Loftis <ben@harrisonconsoles.com>
2024-10-18 11:09:37 -0500

    transferring from ardour_ui (focus_on_clock)

--- gtk2_ardour/transport_bar.h
+++ gtk2_ardour/transport_bar.h
@@ -70,6 +70,8 @@ public:
 
 	void set_session (ARDOUR::Session *);
 
+	void focus_on_clock ();
+
 private:
 	Gtk::Table    transport_table;
 
@@ -127,8 +129,6 @@ private:
 
 	void latency_switch_changed ();
 	void session_latency_updated (bool);
-
-	void focus_on_clock ();
 
 	void update_clock_visibility ();
 
