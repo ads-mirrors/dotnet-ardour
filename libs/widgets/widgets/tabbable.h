idimetrix/ardour

Tabbable overhaul: consistent toplevel packing

libs/widgets/widgets/tabbable.h
@@ -23,11 +23,16 @@
 #include <gtkmm/bin.h>
 #include <gtkmm/box.h>
 #include <gtkmm/button.h>
+#include <gtkmm/eventbox.h>
 #include <gtkmm/image.h>
 #include <gtkmm/label.h>
 #include <gtkmm/notebook.h>
 
 #include "gtkmm2ext/window_proxy.h"
+
+#include "widgets/ardour_button.h"
+#include "widgets/eventboxext.h"
+#include "widgets/pane.h"
 #include "widgets/visibility.h"
 
 namespace Gtk {
@@ -80,13 +85,59 @@ public:
 
 	sigc::signal1<void,Tabbable&> StateChange;
 
+	/* this is where ArdourUI packs the tab switchers
+	* (record/cues/edit/mix) into my toolbar area,
+	* in the case where I'm attached to the main window
+	*/
+	Gtk::EventBox&  content_tabbables_ebox() {return _content_tabbables_ebox;}
+
+	void strip_button_toggled();
+	void list_button_toggled();
+	void props_button_toggled();
+
 protected:
 	bool delete_event_handler (GdkEventAny *ev);
 
 	sigc::signal1<void, bool> signal_tabbed_changed;
 
+	/* This is the heirarchy of a Tabbable's widget packing.
+	 * The end result is to provide 8(ish) event-boxen where the tab can put its contents
+	 * Please maintain the indention here so the hierarchy is visible
+	*/
+
+	/* clang-format off */
+	Gtk::VBox     _content_vbox;                     /* this is the root widget for a full-featured tabbable, which contains:  */
+	/*              toolbar_frame                     * the frame is managed in the implementation */
+	Gtk::HBox       _content_header_hbox;
+	EventBoxExt       _content_transport_ebox;       /* a placeholder for the transport bar, if you want one */
+	Gtk::EventBox     _content_attachments_ebox;     /* a placeholder the (strip, list, props) visibility buttons for this tab */
+	Gtk::HBox           _content_attachment_hbox;
+	EventBoxExt       _content_tabbables_ebox;       /* a placeholder for the tabbable switching buttons (used by ArdourUI) */
+	Gtk::HBox       _content_hbox;
+	EventBoxExt       _content_strip_ebox;           /* a placeholder for the mixer strip, if you want one */
+	Gtk::VBox         _content_midlevel_vbox;
+	HPane               _content_list_pane;
+	Gtk::VBox             _content_inner_vbox;
+	EventBoxExt             _content_toolbar_ebox;   /* a placeholder for the content-specific toolbar, if you want one */
+	Gtk::HBox               _content_innermost_ebox; /* a placeholder for the innermost content (recorder, cues, editor, mixer) */
+	Gtk::VBox             _content_list_vbox;
+	EventBoxExt           _content_list_ebox;        /* a placeholder for the sidebar list, if you want one */
+	EventBoxExt         _content_props_ebox;         /* a placeholder for the property box, if you want one */
+	/* clang-format on */
+
+	Gtk::Widget&   _contents;  //for most Tabbables this will be _content_vbox;  but rc_options, for example, does something different.
+
+	/* visibility controls */
+	ArdourWidgets::ArdourButton _strip_attachment_button;
+	ArdourWidgets::ArdourButton _list_attachment_button;
+	ArdourWidgets::ArdourButton _prop_attachment_button;
+
+	void showhide_sidebar_strip (bool yn);
+	void showhide_sidebar_list (bool yn);
+	void showhide_btm_props (bool yn);
+
 private:
-	Gtk::Widget&   _contents;
+
 	Gtk::Notebook  _own_notebook;
 	Gtk::Notebook* _parent_notebook;
 	bool            tab_requested_by_state;

ads-mirrors/dotnet-ardour

libs/widgets/widgets/tabbable.h
@@ -20,14 +20,18 @@
 #ifndef _WIDGETS_TABBABLE_H_
 #define _WIDGETS_TABBABLE_H_
 
-#include <gtkmm/bin.h>
 #include <gtkmm/box.h>
 #include <gtkmm/button.h>
+#include <gtkmm/eventbox.h>
 #include <gtkmm/image.h>
 #include <gtkmm/label.h>
 #include <gtkmm/notebook.h>
 
 #include "gtkmm2ext/window_proxy.h"
+
+#include "widgets/ardour_button.h"
+#include "widgets/eventboxext.h"
+#include "widgets/pane.h"
 #include "widgets/visibility.h"
 
 namespace Gtk {
@@ -56,6 +60,12 @@ public:
 
 	Gtk::Widget& contents() const { return _contents; }
 
+	/* this is where ArdourUI packs the tab switchers
+	 * (record/cues/edit/mix) into my toolbar area,
+	 * in the case where I'm attached to the main window
+	 */
+	Gtk::EventBox& tab_btn_box () {return content_tabbables_ebox;}
+
 	Gtk::Window* get (bool create = false);
 	Gtk::Window* own_window () { return get (false); }
 	virtual Gtk::Window* use_own_window (bool and_pack_it);
@@ -68,7 +78,6 @@ public:
 	bool tabbed() const;
 	bool tabbed_by_default () const;
 
-
 	Gtk::Window* current_toplevel () const;
 
 	Gtk::Notebook* tab_root_drop ();
@@ -80,23 +89,65 @@ public:
 
 	sigc::signal1<void,Tabbable&> StateChange;
 
+	void strip_button_toggled();
+	void list_button_toggled();
+	void props_button_toggled();
+
 protected:
+	virtual void showhide_sidebar_strip (bool yn);
+	virtual void showhide_sidebar_list (bool yn);
+	virtual void showhide_btm_props (bool yn);
+
 	bool delete_event_handler (GdkEventAny *ev);
 
 	sigc::signal1<void, bool> signal_tabbed_changed;
 
-private:
-	Gtk::Widget&   _contents;
-	Gtk::Notebook  _own_notebook;
-	Gtk::Notebook* _parent_notebook;
-	bool            tab_requested_by_state;
+	/* This is the heirarchy of a Tabbable's widget packing.
+	 * The end result is to provide 8(ish) event-boxen where the tab can put its contents
+	 * Please maintain the indention here so the hierarchy is visible
+	*/
 
+	/* clang-format off */
+	/*            _content_vbox                     * toplevel
+	 *             toolbar_frame                     * the frame is managed in the implementation */
+	Gtk::HBox       content_header_hbox;
+	EventBoxExt       content_transport_ebox;       /* a placeholder for the transport bar, if you want one */
+	Gtk::EventBox     content_attachments_ebox;     /* a placeholder the (strip, list, props) visibility buttons for this tab */
+	Gtk::HBox           content_attachment_hbox;
+	EventBoxExt       content_tabbables_ebox;       /* a placeholder for the tabbable switching buttons (used by ArdourUI) */
+	Gtk::HBox       content_hbox;
+	EventBoxExt       content_strip_ebox;           /* a placeholder for the mixer strip, if you want one */
+	Gtk::VBox         content_midlevel_vbox;
+	HPane               content_list_pane;
+	Gtk::VBox             content_inner_vbox;
+	EventBoxExt             content_toolbar_ebox;   /* a placeholder for the content-specific toolbar, if you want one */
+	Gtk::HBox               content_innermost_ebox; /* a placeholder for the innermost content (recorder, cues, editor, mixer) */
+	Gtk::VBox             content_list_vbox;
+	EventBoxExt           content_list_ebox;        /* a placeholder for the sidebar list, if you want one */
+	EventBoxExt         content_props_ebox;         /* a placeholder for the property box, if you want one */
+	/* clang-format on */
+
+
+	/* visibility controls */
+	ArdourWidgets::ArdourButton strip_attachment_button;
+	ArdourWidgets::ArdourButton list_attachment_button;
+	ArdourWidgets::ArdourButton prop_attachment_button;
+
+private:
+	void default_layout ();
 	void show_tab ();
 	void hide_tab ();
 	bool tab_close_clicked (GdkEventButton*);
 	void show_own_window (bool and_pack_it);
 	void window_mapped ();
 	void window_unmapped ();
+
+	Gtk::VBox      _content_vbox; /* this is the root widget for a full-featured tabbable, which contains:  */
+	Gtk::Widget&   _contents; /* for most Tabbables this will be content_vbox;  but rc_options, for example, does something different. */
+	Gtk::Notebook  _own_notebook;
+	Gtk::Notebook* _parent_notebook;
+	bool            tab_requested_by_state;
+
 };
 
 } /* end namespace */