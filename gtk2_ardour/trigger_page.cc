idimetrix/ardour

trigger page has no sidebar strip (for now)

gtk2_ardour/trigger_page.cc
@@ -80,6 +80,8 @@ TriggerPage::TriggerPage ()
 	_transport_bar = manage(new TransportBar());
 	_transport_bar->show();
 
+	_strip_attachment_button.set_sensitive(false);
+
 	/* Match TriggerStrip::_name_button height */
 	ArdourButton* spacer = manage (new ArdourButton (ArdourButton::Text));
 	spacer->set_name ("mixer strip button");

ads-mirrors/dotnet-ardour

gtk2_ardour/trigger_page.cc
@@ -80,6 +80,8 @@ TriggerPage::TriggerPage ()
 	_transport_bar = manage(new TransportBar());
 	_transport_bar->show();
 
+	strip_attachment_button.set_sensitive(false);
+
 	/* Match TriggerStrip::_name_button height */
 	ArdourButton* spacer = manage (new ArdourButton (ArdourButton::Text));
 	spacer->set_name ("mixer strip button");